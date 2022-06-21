4d639f40e52015f0dbcd8cc6060af3a1ad646169
Wellington <wellington.map@hotmail.com>
2022-06-21 15:50:49 -0300

    chore: config storybook, theme, global styles, and remove unused storybook examples

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  stories: ["../src/components/**/stories.tsx"],
+  addons: ["@storybook/addon-essentials"],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`);
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import "../.jest/__mocks__/nextImageMock";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "styles/global";
+import theme from "styles/theme";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
