f5ab32cb98b2533d8df2f3489701263f66fcee2e
Wellington <wellington.map@hotmail.com>
2022-06-21 21:01:22 -0300

    fix: adjust typecheck and add script to run in pre commit with husky

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,8 @@
-import { createGlobalStyle, css, DefaultTheme } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
 type GlobalStylesProps = {
   removeBg?: boolean;
-} & DefaultTheme;
+};
 
 const GlobalStyles = createGlobalStyle<GlobalStylesProps>`
   @font-face {
